2776d598e9b1a881faf73bec75a9138e2a634d44
alBG23 <alexmsv@gmail.com>
2025-04-03 22:15:05 +0300

    Update configuration for Vercel deployment

---
+++ deploy.ps1
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Update configuration for Vercel deployment"
+git push origin main 
---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git add .
+git commit -m "Update configuration for Vercel deployment"
+git push origin main 
